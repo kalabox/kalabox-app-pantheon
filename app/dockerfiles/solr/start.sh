302cab92b43e227c58fd2858e04bb27b3790bb0c
Mike Pirog <mike@kalamuna.com>
2015-07-04 18:43:26 -0700

    #232: Rebase on tomcat7 and cull the herds

--- app/dockerfiles/solr/start.sh
+++ app/dockerfiles/solr/start.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-
-export CATALINA_HOME=/usr/share/tomcat6
-export CATALINA_BASE=/var/lib/tomcat6
+export CATALINA_HOME=/usr/share/tomcat7
+export CATALINA_BASE=/var/lib/tomcat7
 
 incrond
-/usr/share/tomcat6/bin/catalina.sh run
+/usr/share/tomcat7/bin/catalina.sh run
