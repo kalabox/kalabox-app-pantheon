d1f8c06fd0ca58699d307b42f6fef3236e3caefc
Mike Pirog <mike@kalamuna.com>
2016-01-12 11:54:42 -0800

    Reorganize tasks.js

--- app/plugins/kalabox-plugin-pantheon/lib/events.js
+++ app/plugins/kalabox-plugin-pantheon/lib/events.js
@@ -2,6 +2,9 @@
 
 module.exports = function(kbox) {
 
+  // Kbox modules
+  var events = kbox.core.events.context('6e086c59-20d2-46b5-84ce-44d2de1f58b6');
+
   /*
    * Add our pantheon settings to the ENV
    */
@@ -9,4 +12,42 @@ module.exports = function(kbox) {
     var identifier = 'app_pantheon_config';
     kbox.core.env.setEnvFromObj(app.config.pluginconfig.pantheon, identifier);
   });
+
+  /*
+   * Build the site after post-create happens
+   */
+  events.on('post-create-app', function(app, done) {
+
+    // Get our push and pull stuff
+    var puller = require('./pull.js')(kbox, app);
+
+    // Grab the needed clients
+    var Terminus = require('./terminus.js');
+    var terminus = new Terminus(kbox, app);
+
+    // Our pantheon config for later on
+    var pantheonConf = app.config.pluginConf.pantheon;
+
+    // Get our pantheon site aliases
+    return terminus.getSiteAliases()
+
+    // Pull our code for the first time
+    .then(function() {
+      return puller.pullCode(pantheonConf.site, pantheonConf.env);
+    })
+
+    // Pull our DB
+    .then(function() {
+      return puller.pullDB(pantheonConf.site, pantheonConf.env);
+    })
+
+    // Get our files
+    .then(function() {
+      return puller.pullFiles(pantheonConf.site, pantheonConf.env);
+    })
+
+    .nodeify(done);
+
+  });
+
 };
--- app/plugins/kalabox-plugin-pantheon/lib/tasks.js
+++ app/plugins/kalabox-plugin-pantheon/lib/tasks.js
@@ -3,18 +3,10 @@
 module.exports = function(kbox) {
 
   // Modules
-  var path = require('path');
   var _ = require('lodash');
 
   // Nobody expects the Spanish Inquisition!
   var inquirer = require('inquirer');
-
-  // "Constants"
-  var PLUGIN_NAME = 'kalabox-plugin-pantheon';
-  var TERMINUS = 'terminus:t0.9.3';
-
-  // Kbox modules
-  var events = kbox.core.events.context('6e086c59-20d2-46b5-84ce-44d2de1f58b6');
 
   kbox.whenAppRegistered(function(app) {
 
@@ -25,39 +17,13 @@ module.exports = function(kbox) {
     // Grab the needed clients
     var Terminus = require('./terminus.js');
     var terminus = new Terminus(kbox, app);
-    var pathToRoot = path.resolve(__dirname, '..', '..', '..');
-    var pathToNode = path.join(pathToRoot, 'node_modules');
-    var Drush = require(pathToNode + '/kalabox-plugin-drush/lib/drush.js');
-    var drush = new Drush(kbox, app, TERMINUS, PLUGIN_NAME);
 
     // Get our config
-    var pantheonConf = app.config.pluginConf['kalabox-plugin-pantheon'];
+    var pantheonConf = app.config.pluginConf.pantheon;
 
     // Supports pull env
     var supportedPullEnvs = [pantheonConf.env, 'test', 'live'];
     var supportedPushEnvs = [pantheonConf.env];
-
-    /*
-     * Filter out questions that have been answered interactively
-     */
-    var filterQuestions = function(questions, options) {
-
-      return _.filter(questions, function(question) {
-
-        var option = options[question.name];
-
-        if (question.filter) {
-          options[question.name] = question.filter(options[question.name]);
-        }
-        if (option === false || option === undefined) {
-          return true;
-        }
-        else {
-          return !_.includes(Object.keys(options), question.name);
-        }
-
-      });
-    };
 
     /*
      * Get choices array for supported pull environments
@@ -104,21 +70,8 @@ module.exports = function(kbox) {
       return choices;
     };
 
-    // Tasks
-    // kbox terminus COMMAND
-    kbox.tasks.add(function(task) {
-      task.path = [app.name, 'terminus'];
-      task.category = 'appCmd';
-      task.description = 'Run terminus commands.';
-      task.kind = 'delegate';
-      task.func = function(done) {
-        var opts = terminus.getOpts(this.options);
-        var cmd = this.payload;
-        terminus.cmd(cmd, opts, done);
-      };
-    });
-
     // kbox drush COMMAND
+    /*
     kbox.tasks.add(function(task) {
       task.path = [app.name, 'drush'];
       task.category = 'appCmd';
@@ -150,6 +103,7 @@ module.exports = function(kbox) {
         drush.cmd(cmd, opts, done);
       };
     });
+    */
 
     // kbox pull
     kbox.tasks.add(function(task) {
@@ -248,7 +202,7 @@ module.exports = function(kbox) {
         ];
 
         // Filter out interactive questions based on passed in options
-        questions = filterQuestions(questions, options);
+        questions = kbox.util.cli.filterQuestions(questions, options);
 
         // Launch the inquiry
         inquirer.prompt(questions, function(answers) {
@@ -305,105 +259,8 @@ module.exports = function(kbox) {
       };
     });
 
-    // Set the integrations pull method.
-    kbox.integrations.get('pantheon').setMethod('pull', function() {
-      var self = this;
-      return self.ask([
-        {
-          id: 'shouldPullFiles'
-        },
-        {
-          id: 'shouldPullDatabase'
-        }
-      ])
-      .then(function(answers) {
-        // Grab pantheon config so we can mix in interactives
-        var pantheonConf = app.config.pluginConf['kalabox-plugin-pantheon'];
-        // Grab pantheon aliases
-        return terminus.getSiteAliases()
-        // Pull our code
-        .then(function() {
-          self.update('Pulling code.');
-          return puller.pullCode(pantheonConf.site, pantheonConf.env);
-        })
-        // Pull our DB if selected
-        .then(function() {
-          if (answers.shouldPullDatabase) {
-            self.update('Pulling database.');
-            return puller.pullDB(pantheonConf.site, pantheonConf.env);
-          }
-        })
-        // Pull our files if selected
-        .then(function() {
-          if (answers.shouldPullFiles) {
-            self.update('Pulling files.');
-            return puller.pullFiles(pantheonConf.site, pantheonConf.env);
-          }
-        })
-        .then(function() {
-          self.update('Done pulling.');
-        });
-      });
-    });
-
-    // Set the integrations push method.
-    kbox.integrations.get('pantheon').setMethod('push', function() {
-      // Save reference for later.
-      var self = this;
-      // Get plugin.
-      var pantheonConf = app.config.pluginConf['kalabox-plugin-pantheon'];
-      // Get site aliases.
-      return kbox.Promise.try(function() {
-        return terminus.getSiteAliases();
-      })
-      // Ask questions.
-      .then(function() {
-        return self.ask([
-          {
-            id: 'message'
-          },
-          {
-            id: 'database'
-          },
-          {
-            id: 'files'
-          }
-        ]);
-      })
-      // Push code.
-      .tap(function(answers) {
-        self.update('Pushing code.');
-        return pusher.pushCode(
-          pantheonConf.site,
-          pantheonConf.env,
-          answers.message
-        );
-      })
-      // Push database.
-      .tap(function(answers) {
-        if (answers.database && answers.database !== 'none') {
-          self.update('Pushing database.');
-          return pusher.pushDB(pantheonConf.site, answers.database);
-        }
-      })
-      // Push files.
-      .tap(function(answers) {
-        if (answers.files && answers.files !== 'none') {
-          self.update('Pushing files.');
-          return pusher.pushFiles(pantheonConf.site, answers.files);
-        }
-      })
-      // Signal completion.
-      .tap(function() {
-        self.update('Done pushing.');
-      });
-    });
-
     // kbox appname push
     kbox.tasks.add(function(task) {
-
-      // Grab pantheon conf
-      var pantheonConf = app.config.pluginConf[PLUGIN_NAME];
 
       // Task metadata
       task.path = [app.name, 'push'];
@@ -490,7 +347,7 @@ module.exports = function(kbox) {
         ];
 
         // Filter out interactive questions based on passed in options
-        questions = filterQuestions(questions, options);
+        questions = kbox.util.cli.filterQuestions(questions, options);
 
         // Launch the inquiry
         inquirer.prompt(questions, function(answers) {
@@ -537,35 +394,6 @@ module.exports = function(kbox) {
         });
 
       };
-    });
-
-    // Events
-    // Build the site after post-create happens
-    events.on('post-create-app', function(app, done) {
-
-      // Our pantheon config for later on
-      var pantheonConf = app.config.pluginConf[PLUGIN_NAME];
-
-      // Get our pantheon site aliases
-      return terminus.getSiteAliases()
-
-      // Pull our code for the first time
-      .then(function() {
-        return puller.pullCode(pantheonConf.site, pantheonConf.env);
-      })
-
-      // Pull our DB
-      .then(function() {
-        return puller.pullDB(pantheonConf.site, pantheonConf.env);
-      })
-
-      // Get our files
-      .then(function() {
-        return puller.pullFiles(pantheonConf.site, pantheonConf.env);
-      })
-
-      .nodeify(done);
-
     });
 
   });
