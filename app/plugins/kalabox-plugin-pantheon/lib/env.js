4d358d0a543e0de95c6b6115ad38325bd5b9b729
rileysaurus <riley.cauldwell@gmail.com>
2016-03-25 14:45:32 -0700

    Adding app type guards around app plugin logic.

--- app/plugins/kalabox-plugin-pantheon/lib/env.js
+++ app/plugins/kalabox-plugin-pantheon/lib/env.js
@@ -11,126 +11,131 @@ module.exports = function(kbox) {
 
   kbox.ifApp(function(app) {
 
-    /*
-     * Load our app config
-     */
-    var getConfig = function() {
-      return kbox.util.yaml.toJson(confPath).pluginconfig.pantheon;
-    };
+    // Only run if app type is pantheon.
+    if (app.type === 'pantheon') {
 
-    /*
-     * Factory fucntion to construct our environment based on framework
-     */
-    var getEnv = function(framework) {
-
-      // Framework specific stuff
-      var frameworkSpec = {
-        drupal: {
-          filemount: 'sites/default/files'
-        },
-        drupal8: {
-          filemount: 'sites/default/files'
-        },
-        wordpress: {
-          filemount: 'wp-content/uploads'
-        },
-        backdrop: {
-          filemount: 'files'
-        }
+      /*
+       * Load our app config
+       */
+      var getConfig = function() {
+        return kbox.util.yaml.toJson(confPath).pluginconfig.pantheon;
       };
 
-      // Pressflow database settings
-      // Right now pantheon adds in drupal framework pressflow settings for
-      // all frameworks
-      // @todo: eventually this might change?
-      var pantheonDatabases = {
-        default: {
-          default: {
-            driver: 'mysql',
-            prefix: '',
-            database: 'pantheon',
-            username: 'pantheon',
-            password: 'pantheon',
-            host: 'database',
-            port: 3306
+      /*
+       * Factory fucntion to construct our environment based on framework
+       */
+      var getEnv = function(framework) {
+
+        // Framework specific stuff
+        var frameworkSpec = {
+          drupal: {
+            filemount: 'sites/default/files'
+          },
+          drupal8: {
+            filemount: 'sites/default/files'
+          },
+          wordpress: {
+            filemount: 'wp-content/uploads'
+          },
+          backdrop: {
+            filemount: 'files'
           }
-        }
+        };
+
+        // Pressflow database settings
+        // Right now pantheon adds in drupal framework pressflow settings for
+        // all frameworks
+        // @todo: eventually this might change?
+        var pantheonDatabases = {
+          default: {
+            default: {
+              driver: 'mysql',
+              prefix: '',
+              database: 'pantheon',
+              username: 'pantheon',
+              password: 'pantheon',
+              host: 'database',
+              port: 3306
+            }
+          }
+        };
+
+        var getHash = function(u) {
+          return crypto.createHash('sha256').update(u).digest('hex');
+        };
+
+        // Construct a hashsalt for Drupal 8
+        var drupalHashSalt = getHash(JSON.stringify(pantheonDatabases));
+
+        // Some Default settings
+        var settings = {
+          databases: pantheonDatabases,
+          conf: {
+            pressflow_smart_start: true,
+            pantheon_binding: 'kalabox',
+            pantheon_site_uuid: 'kalabox',
+            pantheon_environment: 'kalabox',
+            pantheon_tier: 'kalabox',
+            pantheon_index_host: 'solr',
+            pantheon_index_port: 449,
+            redis_client_host: 'redis',
+            redis_client_port: 8161,
+            redis_client_password: '',
+            file_public_path: 'sites/default/files',
+            file_private_path: 'sites/default/files/private',
+            file_directory_path: 'site/default/files',
+            file_temporary_path: '/tmp',
+            file_directory_temp: '/tmp',
+            css_gzip_compression: false,
+            js_gzip_compression: false,
+            page_compression: false
+          },
+          drupal_hash_salt: drupalHashSalt,
+          config_directory_name: 'config'
+        };
+
+        // Default environmental variables
+        var env = {
+          framework: framework,
+          docroot: '/',
+          filemount: frameworkSpec[framework].filemount,
+          drupalHashSalt: settings.drupal_hash_salt,
+          dbHost: 'database',
+          dbPort: 3306,
+          dbUser: 'pantheon',
+          dbPassword: '',
+          dbName: 'pantheon',
+          pantheonAccount: getConfig().email,
+          pantheonSite: 'UUID',
+          pantheonSiteName: app.name,
+          pantheonEnvironment: 'kalabox',
+          pressflowSettings: JSON.stringify(settings),
+          cacheHost: settings.conf.redis_client_host,
+          cachePort: settings.conf.redis_client_port,
+          cachePassword: settings.conf.redis_client_password,
+          pantheonIndexHost: settings.conf.pantheon_index_host,
+          pantheonIndexPort: settings.conf.pantheon_index_port,
+          backdropSettings: JSON.stringify(settings),
+          authKey: drupalHashSalt,
+          secureAuthKey: getHash(app.name),
+          loggedInKey: getHash(app.domain),
+          authSalt: getHash(app.domain + framework),
+          secureAuthSalt: getHash(app.name + app.domain),
+          loggedInSalt: getHash(app.name + app.name),
+          nonceSalt: getHash(app.domain + app.domain)
+        };
+
+        return env;
+
       };
 
-      var getHash = function(u) {
-        return crypto.createHash('sha256').update(u).digest('hex');
-      };
+      // Get the framework
+      var framework = getConfig().framework;
 
-      // Construct a hashsalt for Drupal 8
-      var drupalHashSalt = getHash(JSON.stringify(pantheonDatabases));
+      // Set some fun ENV action
+      kbox.core.env.setEnvFromObj(getEnv(framework), 'app_pantheon');
 
-      // Some Default settings
-      var settings = {
-        databases: pantheonDatabases,
-        conf: {
-          pressflow_smart_start: true,
-          pantheon_binding: 'kalabox',
-          pantheon_site_uuid: 'kalabox',
-          pantheon_environment: 'kalabox',
-          pantheon_tier: 'kalabox',
-          pantheon_index_host: 'solr',
-          pantheon_index_port: 449,
-          redis_client_host: 'redis',
-          redis_client_port: 8161,
-          redis_client_password: '',
-          file_public_path: 'sites/default/files',
-          file_private_path: 'sites/default/files/private',
-          file_directory_path: 'site/default/files',
-          file_temporary_path: '/tmp',
-          file_directory_temp: '/tmp',
-          css_gzip_compression: false,
-          js_gzip_compression: false,
-          page_compression: false
-        },
-        drupal_hash_salt: drupalHashSalt,
-        config_directory_name: 'config'
-      };
-
-      // Default environmental variables
-      var env = {
-        framework: framework,
-        docroot: '/',
-        filemount: frameworkSpec[framework].filemount,
-        drupalHashSalt: settings.drupal_hash_salt,
-        dbHost: 'database',
-        dbPort: 3306,
-        dbUser: 'pantheon',
-        dbPassword: '',
-        dbName: 'pantheon',
-        pantheonAccount: getConfig().email,
-        pantheonSite: 'UUID',
-        pantheonSiteName: app.name,
-        pantheonEnvironment: 'kalabox',
-        pressflowSettings: JSON.stringify(settings),
-        cacheHost: settings.conf.redis_client_host,
-        cachePort: settings.conf.redis_client_port,
-        cachePassword: settings.conf.redis_client_password,
-        pantheonIndexHost: settings.conf.pantheon_index_host,
-        pantheonIndexPort: settings.conf.pantheon_index_port,
-        backdropSettings: JSON.stringify(settings),
-        authKey: drupalHashSalt,
-        secureAuthKey: getHash(app.name),
-        loggedInKey: getHash(app.domain),
-        authSalt: getHash(app.domain + framework),
-        secureAuthSalt: getHash(app.name + app.domain),
-        loggedInSalt: getHash(app.name + app.name),
-        nonceSalt: getHash(app.domain + app.domain)
-      };
-
-      return env;
-
-    };
-
-    // Get the framework
-    var framework = getConfig().framework;
-
-    // Set some fun ENV action
-    kbox.core.env.setEnvFromObj(getEnv(framework), 'app_pantheon');
+    }
 
   });
 
--- app/plugins/kalabox-plugin-pantheon/lib/events.js
+++ app/plugins/kalabox-plugin-pantheon/lib/events.js
@@ -14,71 +14,76 @@ module.exports = function(kbox) {
    */
   kbox.whenAppRegistered(function(app) {
 
-    // Set our pantheon stuff into the env
-    var identifier = 'app_pantheon_config';
-    kbox.core.env.setEnvFromObj(app.config.pluginconfig.pantheon, identifier);
+    // Only run this if type of this app is pantheon.
+    if (app.config.type === 'pantheon') {
 
-    /*
-     * Build the site after post-create happens
-     */
-    events.on('post-create-app', function(app, done) {
+      // Set our pantheon stuff into the env
+      var identifier = 'app_pantheon_config';
+      kbox.core.env.setEnvFromObj(app.config.pluginconfig.pantheon, identifier);
 
-      // Get our push and pull stuff
-      var puller = require('./pull.js')(kbox, app);
+      /*
+       * Build the site after post-create happens
+       */
+      events.on('post-create-app', function(app, done) {
 
-      // Our pantheon config for later on
-      var pantheonConf = app.config.pluginconfig.pantheon;
+        // Get our push and pull stuff
+        var puller = require('./pull.js')(kbox, app);
 
-      // Pull the pantheon site screenshot
-      return puller.pullScreenshot(pantheonConf.site, pantheonConf.env)
+        // Our pantheon config for later on
+        var pantheonConf = app.config.pluginconfig.pantheon;
 
-      // Pull our code for the first time
-      .then(function() {
-        return puller.pullCode(pantheonConf.site, pantheonConf.env);
-      })
+        // Pull the pantheon site screenshot
+        return puller.pullScreenshot(pantheonConf.site, pantheonConf.env)
 
-      // Pull our DB
-      .then(function() {
-        return puller.pullDB(pantheonConf.site, pantheonConf.env);
-      })
+        // Pull our code for the first time
+        .then(function() {
+          return puller.pullCode(pantheonConf.site, pantheonConf.env);
+        })
 
-      // Get our files
-      .then(function() {
-        return puller.pullFiles(pantheonConf.site, pantheonConf.env);
-      })
+        // Pull our DB
+        .then(function() {
+          return puller.pullDB(pantheonConf.site, pantheonConf.env);
+        })
 
-      .nodeify(done);
+        // Get our files
+        .then(function() {
+          return puller.pullFiles(pantheonConf.site, pantheonConf.env);
+        })
 
-    });
-
-    /*
-     * We don't want to uninstall our data container on a rebuild
-     * so remove the data container from here
-     *
-     * NOTE: this is a nifty implementation where we inception some events
-     * to target exactly what we want
-     */
-    events.on('pre-app-rebuild', function(app) {
-
-      // We want to edit our engine remove things
-      events.on('pre-engine-destroy', function(data) {
-
-        // Get our services
-        var services = _.flatten(_.map(app.composeCore, function(file)  {
-          return _.keys(kbox.util.yaml.toJson(file));
-        }));
-
-        // Remove the data element
-        var withoutData = _.remove(services, function(service) {
-          return service !== 'data';
-        });
-
-        // Update data to note remove data services on rebuilds
-        data.opts = {services: withoutData};
+        .nodeify(done);
 
       });
 
-    });
+      /*
+       * We don't want to uninstall our data container on a rebuild
+       * so remove the data container from here
+       *
+       * NOTE: this is a nifty implementation where we inception some events
+       * to target exactly what we want
+       */
+      events.on('pre-app-rebuild', function(app) {
+
+        // We want to edit our engine remove things
+        events.on('pre-engine-destroy', function(data) {
+
+          // Get our services
+          var services = _.flatten(_.map(app.composeCore, function(file)  {
+            return _.keys(kbox.util.yaml.toJson(file));
+          }));
+
+          // Remove the data element
+          var withoutData = _.remove(services, function(service) {
+            return service !== 'data';
+          });
+
+          // Update data to note remove data services on rebuilds
+          data.opts = {services: withoutData};
+
+        });
+
+      });
+
+    }
 
   });
 
