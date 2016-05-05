345ad2029b186432e6b3190f3047f9fb70906247
Mike Pirog <mike@kalamuna.com>
2016-05-05 15:58:29 -0400

    #1223: Refactor app and add deployment

--- ci/travis-ci.sh
+++ ci/travis-ci.sh
@@ -26,13 +26,6 @@ before-install() {
 
   echo "DOCKER_HOST: ${DOCKER_HOST}"
 
-  # Add our key
-  if [ $TRAVIS_PULL_REQUEST == "false" ] &&
-    [ -z "$TRAVIS_TAG" ] &&
-    [ $TRAVIS_REPO_SLUG == $PLUGIN_REPO ] &&
-    [ $TRAVIS_NODE_VERSION == "4.2" ]; then
-    openssl aes-256-cbc -K $encrypted_a3de5a85a96e_key -iv $encrypted_a3de5a85a96e_iv -in ci/travis.id_rsa.enc -out $HOME/.ssh/travis.id_rsa -d
-  fi
 }
 
 # before-script
@@ -43,6 +36,11 @@ before-script() {
   # Global install some npm
   npm install -g grunt-cli
   npm install -g npm
+
+  # Install the app deps
+  cd ${TRAVIS_BUILD_DIR}/app
+  npm install --production
+  cd ${TRAVIS_BUILD_DIR}
 
   # Install kalabox
   sudo apt-get -y update
@@ -94,58 +92,7 @@ after-script() {
 # Clean up after the tests.
 #
 after-success() {
-  # Check for correct travis conditions aka
-  # 1. Is not a pull request
-  # 2. Is not a "travis" tag
-  # 3. Is correct slug
-  # 4. Is latest node version
-  if [ $TRAVIS_PULL_REQUEST == "false" ] &&
-    [ -z "$TRAVIS_TAG" ] &&
-    [ $TRAVIS_REPO_SLUG == $PLUGIN_REPO ] &&
-    [ $TRAVIS_NODE_VERSION == "4.2" ]; then
-
-    # Try to grab our git tag
-    DISCO_TAG=$(git describe --contains HEAD)
-    echo $DISCO_TAG
-    # Grab our package.json version
-    BUILD_VERSION=$(node -pe 'JSON.parse(process.argv[1]).version' "$(cat $TRAVIS_BUILD_DIR/package.json)")
-    echo $BUILD_VERSION
-
-    # SET UP SSH THINGS
-    eval "$(ssh-agent)"
-    chmod 600 $HOME/.ssh/travis.id_rsa
-    ssh-add $HOME/.ssh/travis.id_rsa
-    git config --global user.name "Kala C. Bot"
-    git config --global user.email "kalacommitbot@kalamuna.com"
-
-    # Reset upstream so we can push our changes to it
-    # We need to re-add this in because our clone was originally read-only
-    git remote rm origin
-    git remote add origin git@github.com:$TRAVIS_REPO_SLUG.git
-    git checkout $TRAVIS_BRANCH
-
-    if [ -z "$DISCO_TAG" ]; then
-      # If we are on the master branch then we need to grab the dev
-      # releases of packages when we build our app deps later on
-      export KALABOX_DEV=true
-      echo $KALABOX_DEV
-    fi
-
-    # Go into app and build out the deps so all kalabox needs to do is grab the
-    # package and extract without doing messy things like npm install frmo kbox
-    cd $TRAVIS_BUILD_DIR/app
-    rm -rf node_modules
-    npm install --production
-    cd $TRAVIS_BUILD_DIR
-
-    # Commit our new app deps
-    git add --all
-    git commit -m "BUILT OUT APP DEPS [ci skip]" --author="Kala C. Bot <kalacommitbot@kalamuna.com>" --no-verify
-
-    # Push up our generated app deps plus a tag if we also have a new version
-    git push origin $TRAVIS_BRANCH --tags
-
-  fi
+  echo
 }
 
 # before-deploy
@@ -153,7 +100,28 @@ after-success() {
 # Clean up after the tests.
 #
 before-deploy() {
-  echo
+
+  # THis is a production release!
+  if [ $TRAVIS_PULL_REQUEST == "false" ] &&
+    [ ! -z "$TRAVIS_TAG" ] &&
+    [ $TRAVIS_REPO_SLUG == "$PLUGIN_REPO" ]; then
+
+    # Do a production build
+    grunt pkg
+    mkdir -p prod_build
+    mv dist/kalabox-app-pantheon* prod_build/kalabox-app-pantheon-$TRAVIS_TAG.tar.gz
+
+  fi
+
+  # Do the build again for our dev releases
+  grunt pkg --dev
+
+  # Rename our build and produce a latest build
+  mkdir -p dev_build
+
+  # Rename the build
+  cp dist/kalabox-app-pantheon* dev_build/kalabox-app-pantheon-latest.tar.gz
+
 }
 
 # after-deploy
---
+++ index.js
@@ -0,0 +1,141 @@
+
+'use strict';
+
+module.exports = function(kbox) {
+
+  // Node modules
+  var path = require('path');
+
+  // NPM modules
+  var _ = require('lodash');
+  var VError = require('verror');
+
+  // Grab client module
+  var Client = require('./lib/client.js');
+  var pantheon = new Client(kbox);
+
+  // Load our events and create modules
+  require('./lib/create.js')(kbox, pantheon);
+  require('./lib/events.js')(kbox, pantheon);
+
+  /*
+   * App package location.
+   * NOTE: if in dev mode and not in a binary this should be internal. Internal
+   * locations are relative to the node_modules folder
+   * All
+   * other modes should be an URL to an archive and then the path location of the app
+   * relative to the archive root
+   */
+  var packageData = function() {
+
+    // Get relevant config options
+    var config = require(path.join(__dirname, 'lib', 'config.json'));
+    var isBinary = kbox.core.deps.get('globalConfig').isBinary;
+    var locked = kbox.core.deps.get('globalConfig').locked;
+
+    // Return an internal path
+    if (!isBinary && !locked) {
+      var srcRoot = kbox.core.deps.get('globalConfig').srcRoot;
+      var modFold = path.join(srcRoot, 'node_modules');
+      return {
+        path: path.join(modFold, config.path.base, config.path.path)
+      };
+    }
+    // Return a url of an archive
+    else {
+      var branch = (!locked) ? config.url.dev : config.url.version;
+      var url = [config.url.base, 'tarball', 'v' + branch].join('/');
+      return {
+        url: url,
+        path: config.url.path,
+        folder: config.url.folder
+      };
+    }
+
+  };
+
+  // Declare our app to the world
+  kbox.create.add('pantheon', {
+    task: {
+      name: 'Pantheon',
+      description: 'Creates a Pantheon app.',
+      pkg: packageData()
+    }
+  });
+
+  // Task to create kalabox apps
+  kbox.tasks.add(function(task) {
+    kbox.create.buildTask(task, 'pantheon');
+  });
+
+  // Create integration.
+  kbox.integrations.create('pantheon', function(api) {
+
+    // Authorize login.
+    api.methods.auth = function(username, password) {
+      pantheon.reset();
+      return pantheon.auth(username, password)
+      .wrap('Error authorizing: %s', username);
+    };
+
+    // Set the logins method of api.
+    api.methods.logins = function() {
+      return kbox.Promise.try(function() {
+        pantheon.reset();
+        return pantheon.getSessionFiles();
+      })
+      .wrap('Error getting logins.');
+    };
+
+    // Set the sites method of the api.
+    api.methods.sites = function(username) {
+      // Get email.
+      // Set session based on email.
+      return kbox.Promise.try(function() {
+        pantheon.reset();
+        var session = pantheon.getSessionFile(username);
+        pantheon.setSession(username, session);
+      })
+      // Get and map sites.
+      .then(function() {
+        return pantheon.getSites()
+        .then(function(sites) {
+          return _.transform(sites, function(acc, val, key) {
+            acc.push({
+              name: val.information.name,
+              id: key,
+              // Instead of loading environments now, instead create a function
+              // that can be called to get them when needed.
+              getEnvironments: function() {
+                // Reset cache.
+                pantheon.reset();
+                var session = pantheon.getSessionFile(username);
+                pantheon.setSession(username, session);
+                // Get environments.
+                return pantheon.getEnvironments(key)
+                // Wrap errors.
+                .catch(function(err) {
+                  throw new VError(err, 'Error getting environments.');
+                })
+                // Map, filter, and sort environments.
+                .then(function(envs) {
+                  delete envs.live;
+                  delete envs.test;
+                  envs = _.map(envs, function(val, key) {
+                    return key;
+                  });
+                  envs.sort();
+                  return envs;
+                });
+              }
+            });
+            return acc;
+          }, []);
+        });
+      })
+      .wrap('Error getting sites.');
+    };
+
+  });
+
+};
