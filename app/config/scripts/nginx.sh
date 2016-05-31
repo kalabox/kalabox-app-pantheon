9508145667195db9c0209958ccbb85cb7c94a77e
Mike Pirog <mike@kalamuna.com>
2016-05-31 16:00:32 -0700

    #1333: Basic fucnctional decoupled appserver

---
+++ app/config/scripts/nginx.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -e
+
+# Set defaults
+: ${KALABOX_UID:='1000'}
+: ${KALABOX_GID:='50'}
+
+# Correctly map users
+echo "Remapping permissions for VB sharing compat..."
+usermod -u "$KALABOX_UID" www-data
+groupmod -g "$KALABOX_GID" www-data || usermod -G staff www-data
+
+# Make sure we have correct ownership
+chown -Rf www-data:www-data /code
+chown -Rf www-data:www-data /media
+chown -Rf www-data:www-data /php
+
+# Set up our certs for the appserver with nginx
+if [ ! -f "/certs/appserver.pem" ]; then
+  openssl genrsa -out /certs/appserver.key 2048 && \
+  openssl req -new -x509 -key /certs/appserver.key -out /certs/appserver.crt -days 365 -subj "/C=US/ST=California/L=San Francisco/O=Kalabox/OU=KB/CN=appserver" && \
+  cat /certs/appserver.crt /certs/appserver.key > /certs/appserver.pem
+fi
+
+# Wait until our solr crt is ready and then whitelist it
+NEXT_WAIT_TIME=0
+until [ -f /certs/binding.crt ] || [ $NEXT_WAIT_TIME -eq 5 ]; do
+  echo "Waiting for solr certs to be set up..."
+  sleep $(( NEXT_WAIT_TIME++ ))
+done
+mkdir -p /usr/share/ca-certificates/solr
+cp /certs/binding.crt /usr/share/ca-certificates/solr/binding.crt
+echo "solr/binding.crt" >> /etc/ca-certificates.conf
+update-ca-certificates --fresh
+
+# Run the NGINX
+nginx "$@"
---
+++ app/config/scripts/php-fpm.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+# Set defaults
+: ${KALABOX_UID:='1000'}
+: ${KALABOX_GID:='50'}
+
+# Correctly map users
+echo "Remapping permissions for VB sharing compat..."
+usermod -u "$KALABOX_UID" www-data
+usermod -G staff www-data || groupmod -g "$KALABOX_GID" www-data || true
+
+# Make sure we have correct ownership
+chown -Rf www-data:www-data /code
+chown -Rf www-data:www-data /media
+chown -Rf www-data:www-data /php
+
+# Run fpm with command options
+php-fpm "$@"
