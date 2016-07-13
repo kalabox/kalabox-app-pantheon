da515350dc4eb26da1fbb46bb472d1e74f743184
Mike Pirog <mike@kalamuna.com>
2016-07-13 00:08:26 -0700

    #1396: Remove old SSHKEY setup

---
+++ app/config/scripts/pantheon-keys.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+# Set defaults
+: ${KALABOX_UID:='1000'}
+: ${KALABOX_GID:='50'}
+: ${KALABOX_SSH_KEY:='pantheon.kalabox.id_rsa'}
+
+#
---
+++ app/config/scripts/usermap-cli.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+# Set defaults
+: ${KALABOX_UID:='1000'}
+: ${KALABOX_GID:='50'}
+: ${KALABOX_SSH_KEY:='pantheon.kalabox.id_rsa'}
+
+# Add local user to match host
+addgroup "$KALABOX_GID" || true
+adduser -D -h "$HOME" -G "$KALABOX_GID" "$KALABOX_UID"
+
+# Check for an SSH key and make it has the correc permissions
+# We need to do this because VB SHARING ON WINDOZE may set the key as
+# 777
+if [ -f "$HOME/.ssh/$KALABOX_SSH_KEY" ]; then
+  chmod 700 $HOME/.ssh
+  chmod 600 $HOME/.ssh/$KALABOX_SSH_KEY
+  chown -Rf $KALABOX_UID:$KALABOX_UID $HOME/.ssh
+fi
+
+# Run the command
+echo "$KALABOX_UID ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+su -m "$KALABOX_UID" -s "/bin/bash" -c "$(echo $@)"
