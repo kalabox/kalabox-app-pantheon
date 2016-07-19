b3a01d3d078506a45dcbf5ad8dfd4cbb70fa9dd6
Mike Pirog <mike@kalamuna.com>
2016-07-18 21:54:34 -0700

    #1455: simplify integration points

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ module.exports = function(kbox) {
 
   // NPM modules
   var _ = require('lodash');
-  var VError = require('verror');
 
   // Grab client module
   var Client = require('./lib/client.js');
@@ -18,29 +17,14 @@ module.exports = function(kbox) {
   require('./lib/create.js')(kbox, pantheon);
   require('./lib/events.js')(kbox, pantheon);
 
-  /*
-   * App package location.
-   * NOTE: if in dev mode and not in a binary this should be internal. Internal
-   * locations are relative to the node_modules folder
-   * All
-   * other modes should be an URL to an archive and then the path location of the app
-   * relative to the archive root
-   */
-  var packageData = function() {
-
-    // Return our pkg data
-    return {
-      dir: path.join(__dirname, 'app')
-    };
-
-  };
-
   // Declare our app to the world
   kbox.create.add('pantheon', {
     task: {
       name: 'Pantheon',
       description: 'Creates a Pantheon app.',
-      pkg: packageData()
+      pkg: {
+        dir: path.join(__dirname, 'app')
+      }
     }
   });
 
@@ -53,65 +37,28 @@ module.exports = function(kbox) {
   kbox.integrations.create('pantheon', function(api) {
 
     // Authorize login.
-    api.methods.auth = function(username, password) {
-      pantheon.reset();
-      return pantheon.auth(username, password)
-      .wrap('Error authorizing: %s', username);
+    api.methods.auth = function(token) {
+      return pantheon.__auth(token)
+      .wrap('Error authorizing with Machine Token');
     };
 
     // Set the logins method of api.
     api.methods.logins = function() {
       return kbox.Promise.try(function() {
-        pantheon.reset();
-        return pantheon.getSessionFiles();
+        return pantheon.getTokensByEmail();
       })
       .wrap('Error getting logins.');
     };
 
     // Set the sites method of the api.
-    api.methods.sites = function(username) {
-      // Get email.
-      // Set session based on email.
-      return kbox.Promise.try(function() {
-        pantheon.reset();
-        var session = pantheon.getSessionFile(username);
-        pantheon.setSession(username, session);
-      })
-      // Get and map sites.
-      .then(function() {
-        return pantheon.getSites()
-        .then(function(sites) {
-          return _.transform(sites, function(acc, val, key) {
-            acc.push({
-              name: val.information.name,
-              id: key,
-              // Instead of loading environments now, instead create a function
-              // that can be called to get them when needed.
-              getEnvironments: function() {
-                // Reset cache.
-                pantheon.reset();
-                var session = pantheon.getSessionFile(username);
-                pantheon.setSession(username, session);
-                // Get environments.
-                return pantheon.getEnvironments(key)
-                // Wrap errors.
-                .catch(function(err) {
-                  throw new VError(err, 'Error getting environments.');
-                })
-                // Map, filter, and sort environments.
-                .then(function(envs) {
-                  delete envs.live;
-                  delete envs.test;
-                  envs = _.map(envs, function(val, key) {
-                    return key;
-                  });
-                  envs.sort();
-                  return envs;
-                });
-              }
-            });
-            return acc;
-          }, []);
+    api.methods.sites = function(token) {
+      return pantheon.getSites()
+      .then(function(sites) {
+        return _.map(sites, function(site) {
+          site.getEnvironments = function() {
+            return pantheon.getEnvs(token, site.name);
+          };
+          return site;
         });
       })
       .wrap('Error getting sites.');
