dc7699b6f1b9a40f171f08c3ca89dc2b90ad6bd5
Mike Pirog <mike@kalamuna.com>
2016-10-03 20:44:12 -0700

    #1615: Skip merge message edits and default to upstream changes on kbox pull

--- app/config/scripts/ensure-git-dir.sh
+++ app/config/scripts/ensure-git-dir.sh
@@ -6,7 +6,6 @@ set -e
 : ${KALABOX_UID:='1000'}
 : ${KALABOX_GID:='50'}
 
-
 # Set potentially rooted perms back to the correct owner
 if [ -f "/code/.git" ]; then
   sudo chown $KALABOX_UID:$KALABOX_GID -R /code/.git
