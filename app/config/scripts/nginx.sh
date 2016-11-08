a5f477513b6c496a59324172bc5855b2e3de484d
Mike Pirog <mike@kalamuna.com>
2016-11-07 16:44:28 -0800

    #1694: Eine kleine cleanup

--- app/config/scripts/nginx.sh
+++ app/config/scripts/nginx.sh
@@ -25,7 +25,7 @@ if [ ! -f "/certs/appserver.pem" ]; then
   openssl genrsa -out /certs/appserver.key 2048 && \
   openssl req -new -x509 -key /certs/appserver.key -out /certs/appserver.crt -days 365 -subj "/C=US/ST=California/L=San Francisco/O=Kalabox/OU=KB/CN=appserver" && \
   cat /certs/appserver.crt /certs/appserver.key > /certs/appserver.pem
- fi
+fi
 
 # Run the NGINX
 nginx "$@"
